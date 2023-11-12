f0ba5a79812bf8d812aef24712a6de138913f779
Edward <edwardsmakov@gmail.com>
2023-11-12 14:39:50 -0500

    Parse bencoded dictionaries

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"reflect"
 	"strconv"
 	"strings"
 )
@@ -16,7 +15,8 @@ const (
     btStr = "btStr"
     btListStart = "btListStart"
     btListEnd = "btListEnd"
-    btDictStart = "btListEnd"
+    btDictStart = "btDictStart"
+    btDictKey = "btDictKey"
     btDictEnd = "btDictEnd"
 )
 
@@ -34,21 +34,61 @@ func die(e error) {
     }
 }
 
+// Credit: Michael Green
+// https://stackoverflow.com/questions/28541609/looking-for-reasonable-stack-implementation-in-golang
+type stack[T any] struct {
+    Push func(T)
+    Pop func() T
+    Peek func() T
+    Length func() int
+}
+
+func Stack[T any]() stack[T] {
+    slice := make([]T, 0)
+    return stack[T]{
+        Push: func(i T) {
+            slice = append(slice, i)
+        },
+        Pop: func() T {
+            if len(slice) == 0 {
+                return *new(T)
+            }
+            res := slice[len(slice)-1]
+            slice = slice[:len(slice)-1]
+            return res
+        },
+        Peek: func() T {
+            if len(slice) == 0 {
+                return *new(T)
+            }
+
+            res := slice[len(slice)-1]
+            return res
+            
+        },
+        Length: func() int {
+            return len(slice)
+        },
+    }
+}
+
 func main() {
     if len(os.Args) != 2 {
-        die(fmt.Errorf("USAGE: bittorrent-client [.torrent FILENAME]"))
+        die(fmt.Errorf("USAGE: bittorrent-client [*.torrent]"))
     }
     fh, err := os.Open(os.Args[1])
     die(err)
     defer fh.Close()
 
-    // rdr := bufio.NewReader(fh)
     // scan := bufio.NewScanner(fh)
-    scan := bufio.NewScanner(strings.NewReader("l4:spam4:eggsl5:bagell3:loxeee"))
+    scan := bufio.NewScanner(strings.NewReader("d3:keyli1e3:stre10:anotherkeyd3:keyli3ei4ee3:key3:valeeld3:keyli1eed3:key3:val3:keyi2eeee"))
+    // scan := bufio.NewScanner(strings.NewReader("ld3:keyi1234eed3:key3:valee"))
+
     var tokenList = []btToken{}
     line := 0
     indentLevel := 0
-    var lastOpenedType btTokenType
+    shouldExpectDictKey := false
+    compoundTypesEncountered := Stack[btToken]()
 
     tokenizer := func(data []byte, atEOF bool) (advance int, token []byte, err error) {
         for i := 0; i < len(data); i++ {
@@ -61,16 +101,17 @@ func main() {
             case '\t':
                 return 1, data[:i], nil
             case 'l':
-                lastOpenedType = btListStart
-                t := btToken{btListStart, "", "", line, indentLevel}
+                t := btToken{btListStart, "l", "", line, indentLevel}
+                compoundTypesEncountered.Push(t)
                 tokenList = append(tokenList, t)
                 indentLevel++
                 return 1, data[:i], nil
             case 'd':
-                indentLevel++
-                lastOpenedType = btDictStart
-                t := btToken{btDictStart, "", "", line, indentLevel}
+                t := btToken{btDictStart, "d", "", line, indentLevel}
+                compoundTypesEncountered.Push(t)
                 tokenList = append(tokenList, t)
+                indentLevel++
+                shouldExpectDictKey = true
                 return 1, data[:i], nil
             case 'i':
                 curr := i + 1 // Consume i
@@ -84,27 +125,40 @@ func main() {
                 numVal, err := strconv.Atoi(lexeme)
                 die(err)
 
+                if compoundTypesEncountered.Peek().tokenType == "btDictStart" {
+                    // This number was a value in a dict
+                    shouldExpectDictKey = true
+                }
+
                 t := btToken{btNum, lexeme, numVal, line, indentLevel}
                 tokenList = append(tokenList, t)
                 return curr + 1, token, nil
             case 'e':
-                // fmt.Println("saw an E with", string(data), "remaining")
-                indentLevel--;
-                switch lastOpenedType {
+                indentLevel--
+
+                switch compoundTypesEncountered.Pop().tokenType {
                 case "btListStart":
-                    t := btToken{btListEnd, "", "", line, indentLevel}
+                    if compoundTypesEncountered.Peek().tokenType == "btDictStart" {
+                        // This list was a value in a dict
+                        shouldExpectDictKey = true
+                    }
+
+                    t := btToken{btListEnd, "e", "", line, indentLevel}
                     tokenList = append(tokenList, t)
                     return 1, data[:i], nil
                  case "btDictStart": 
-                    t := btToken{btDictEnd, "", "", line, indentLevel}
-                    tokenList = append(tokenList, t)
-                    return 1, data[:i], nil
+                        if compoundTypesEncountered.Peek().tokenType == "btDictStart" {
+                            shouldExpectDictKey = true
+                        }
+
+                        t := btToken{btDictEnd, "", "", line, indentLevel}
+                        tokenList = append(tokenList, t)
+                        return 1, data[:i], nil
                 default:
-                    fmt.Println("couldn't match type", reflect.TypeOf(lastOpenedType).String())
+                    die(fmt.Errorf("Couldn't match type: %v\n", compoundTypesEncountered.Peek()))
                 }
             default:
                 if c >= '0' && c <= '9' {
-                    // fmt.Println("saw a string")
                     curr := i
                     for data[curr] != ':' {
                         curr++
@@ -117,16 +171,28 @@ func main() {
 
                     token := data[tokenStartIdx:tokenEndIdx]
                     lexeme := string(token)
-                    
-                    t := btToken{btStr, lexeme, lexeme, line, indentLevel}
+                    var t btToken
+
+                    if compoundTypesEncountered.Peek().tokenType == "btDictStart" {
+                        if shouldExpectDictKey {
+                            t = btToken{btDictKey, lexeme, lexeme, line, indentLevel}
+                            shouldExpectDictKey = false
+                        } else {
+                            t = btToken{btStr, lexeme, lexeme, line, indentLevel}
+                            shouldExpectDictKey = true
+                        }
+                    } else {
+                        t = btToken{btStr, lexeme, lexeme, line, indentLevel}
+                    }
+
                     tokenList = append(tokenList, t)
                     return tokenEndIdx - i, token, nil
                 } else {
                     fmt.Println("Unrecognized token type")
                     return i + 1, data[:i], nil
                 }
-            }
         }
+    }
 
         if !atEOF {
             return 0, nil, nil
@@ -135,6 +201,9 @@ func main() {
         return 0, data, bufio.ErrFinalToken
     }
     scan.Split(tokenizer)
+
+    fmt.Println("--------------------------------")
+
     for scan.Scan() {
     }
 
@@ -142,6 +211,6 @@ func main() {
         for i := v.indentLevel; i > 0; i-- {
             fmt.Print("\t")
         }
-        fmt.Printf("%v\n", v)
+        fmt.Println(v)
     }
 }
